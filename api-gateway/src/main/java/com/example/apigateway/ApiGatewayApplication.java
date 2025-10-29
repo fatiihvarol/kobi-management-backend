e376b34eeb567149a52fcb06384714e8acf346f3
fatiihvarol <fatihvrl38@gmail.com>
2025-10-29 21:04:01 +0300

    Implement API Gateway rate limiting

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,9 +2,12 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -18,10 +21,24 @@ public class ApiGatewayApplication {
         return builder.routes()
                 .route(p -> p
                         .path("/api/auth/**")
+                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter()).setKeyResolver(keyResolver())))
                         .uri("http://auth-service:8080"))
                 .route(p -> p
                         .path("/api/email/**")
+                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter()).setKeyResolver(keyResolver())))
                         .uri("http://email-service:8081"))
                 .build();
+    }
+
+    @Bean
+    public RedisRateLcimiter redisRateLimiter() {
+        return new RedisRateLimiter(10, 20);
+    }
+
+    @Bean
+    public KeyResolver keyResolver() {
+        return exchange -> exchange.getPrincipal()
+                .map(principal -> principal.getName())
+                .defaultIfEmpty(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
     }
 }
